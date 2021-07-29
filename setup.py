f9d61e6f3d93b987a18416e957e04a4adb9d4128
Matthew Holland <mtoholland@gmail.com>
2021-07-29 11:21:13 +0100

    Updated version number in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(name='malt',
       packages=['malt'],
-      version='1.0.0',
+      version='1.0.1',
       license='MIT',
       author='Matthew Holland',
       description='A package for converting pdb files into TRIPOS MOL2 files')
