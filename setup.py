f79a40d7f7531bcd051a0016f08ef2408d28b6ad
Matthew Holland <mtoholland@gmail.com>
2021-08-24 11:15:00 +0100

    Slight refactoring, and incorporation of electroshape functionality

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup, find_packages
+from Cython.Build import cythonize
+from setuptools.extension import Extension
+import sys
+import os
+
+extensions = [Extension('esp_gen', ['malt/esp_gen.pyx'])]
 
 
 setup(name='malt',
       packages=['malt'],
       include_package_data=True,
-      version='2.2.3',
+      ext_modules=cythonize(extensions, language_level="3", annotate=True),
+      version='2.2.4',
       license='MIT',
       author='Matthew Holland',
       description='A package for converting pdb files into TRIPOS MOL2 files')
